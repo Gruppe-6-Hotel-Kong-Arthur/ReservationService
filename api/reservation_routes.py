c14242af099cd4f3f63b4392777d24ad266159aa
marcus_rk <makj0005@stud.kea.dk>
2024-10-28 11:02:46 +0100

    added api endpoint

--- api/reservation_routes.py
+++ api/reservation_routes.py
@@ -1,5 +1,13 @@
 from flask import Blueprint, jsonify, request
-from repositories.reservation_repository import db_get_reservations
+import os, requests
+from repositories.reservation_repository import (
+    db_get_reservations, 
+    db_get_reservation,
+    db_make_reservation,
+)
+
+GUEST_SERVICE_URL = os.getenv('GUEST_SERVICE_URL')
+ROOM_INVENTORY_SERVICE_URL = os.getenv('ROOM_INVENTORY_SERVICE_URL')
 
 # Blueprint for reservation routes
 reservation_routes = Blueprint('reservation_routes', __name__)
@@ -25,7 +33,32 @@ def new_reservation():
 
     if not guest_id or not room_id or not season_id or not start_date or not end_date:
         return jsonify({"error": "Missing required fields"}), 400
-    
-    
 
-    
+    # make reservation
+    db_make_reservation(guest_id, room_id, season_id, start_date, end_date)
+
+# GET a reservation by id
+@reservation_routes.route('/<int:reservation_id>', methods=['GET'])
+def get_reservation(reservation_id):
+    try:
+        reservation = db_get_reservation(reservation_id)
+
+        # Make a request to the guest service to get guest 
+        guest_information = requests.get(f'{GUEST_SERVICE_URL}/api/v1/guests/{reservation["guest_id"]}')
+
+        # Make a request to the room inventory service to get room information
+        room_information = requests.get(f'{ROOM_INVENTORY_SERVICE_URL}/api/v1/rooms/{reservation["room_id"]}')
+
+        response = {
+            "reservation_id": reservation_id,
+            "first_name": guest_information["first_name"],
+            "last_name": guest_information["last_name"],
+            "country": guest_information["country"],
+            "room_id": reservation["room_id"],
+            "room_type": room_information["room_type"],
+            "price": room_information["price"]
+        }
+
+        return jsonify(response), 200 if response else 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
--- repositories/reservation_repository.py
+++ repositories/reservation_repository.py
@@ -10,3 +10,25 @@ def db_get_reservations():
 
     connection.close()
     return reservations
+
+def db_get_reservation(reservation_id):
+    connection = create_connection()
+
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM Reservations WHERE id = ?', (reservation_id,))
+
+    reservation = cursor.fetchone()
+
+    connection.close()
+    return reservation
+
+
+def db_make_reservation(guest_id, room_id, season_id, start_date, end_date):
+    connection = create_connection()
+
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO Reservations (guest_id, room_id, season_id, start_date, end_date) VALUES (?, ?, ?, ?, ?)',
+                    (guest_id, room_id, season_id, start_date, end_date))
+
+    connection.commit()
+    connection.close()
